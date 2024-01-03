4b60981a8e77184cc8ec8098712488439fe21dfb
Sasikumar Saalujan <126445229+Saalujan@users.noreply.github.com>
2024-01-03 21:05:51 +0530

    sidebar created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { AddCustomer } from "./components/AddCustomer/AddCustomer";
+import { CustomerDetails } from './components/CustomerDetails/CustomerDetails';
+import { CustomersList } from './components/CustomersList/CustomersList';
+import { ReportsInsights } from './components/ReportsInsights/ReportsInsights';
+import { Settings } from './components/Settings/Settings';
+import { Chatbox } from './components/Chatbox/Chatbox';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/AddCustomer" element={<AddCustomer />} />
+        <Route path="/CustomerDetails" element={<CustomerDetails />} />
+        <Route path="/CustomersList" element={<CustomersList />} />
+        <Route path="/ReportsInsights" element={<ReportsInsights />} />
+        <Route path="/Settings" element={<Settings />} />
+        <Route path="/Chatbox" element={<Chatbox />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
---
+++ frontend/src/components/AddCustomer/AddCustomer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Layout from '../../layout/layout'
+
+export const AddCustomer = () => {
+  return (
+    <Layout>
+      <h2>AddCustomer</h2>
+    </Layout>
+  )
+}
