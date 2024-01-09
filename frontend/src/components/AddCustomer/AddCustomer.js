be9f65382bcd8f17ce0d66551f44761c25698d95
Sasikumar Saalujan <126445229+Saalujan@users.noreply.github.com>
2024-01-09 22:10:28 +0530

    addcustomer form created

--- frontend/src/components/AddCustomer/AddCustomer.js
+++ frontend/src/components/AddCustomer/AddCustomer.js
@@ -1,65 +1,110 @@
 import React from 'react'
 import Layout from '../../layout/layout'
+import CommonInput from '../common/CommonInput/CommonInput'
+import img from '../../Assets/Remote meeting-pana.png'
+
 
 export const AddCustomer = () => {
   return (
     <Layout>
-      <div className="d-flex mb-4 Category-AddedSection">
-        <div className="col-6">
-          <h5>Add Customer</h5>
-        </div>
-      </div>
-      <div className='row mb-2'>
-        <div className='col-3'>
-        </div>
 
-        <div className='col'>
-          <div className="container mb-2 AddCustomer-Form-section">
-            <div className="row mb-2">
-              <div className="col-lg-3 col-md-6 col-sm-12">
-                <div className="form-outline mb-2">
-                  <label className="form-label" htmlFor="inputAddAttribute-value">
-                    First Name
-                  </label>
-                  <input
-                    type="text"
-                    id="inputAddAttribute-value"
-                    className="form-control"
-                    required
-                  />
-                </div>
+      <div className='container-fluid p-5 pt-2' style={{color:'gray'}}>
+        <h5>Add Customer</h5>
+        <div className='row addCustomer-container'>
+      
+        <div className='col-md-5 d-flex align-items-center justify-content-center' >
+          <img className={"w-100 d-none d-md-block"} src={img} alt="" />
+          </div>
+          <div className='col-md p-3 mt-1'>
+
+            <div className="row">
+              <div className='col-sm-6 mt-4'>
+                <CommonInput
+                  Label={'First Name'}
+                  Name={'fname'}
+                  PlaceHolder={'Enter your first name'}
+
+                />
               </div>
-              <div className="col-lg-3 col-md-6 col-sm-12">
-                <div className="form-outline mb-2">
-                  <label className="form-label" htmlFor="inputAddAttribute-value">
-                    Last Name
-                  </label>
-                  <input
-                    type="text"
-                    id="inputAddAttribute-value"
-                    className="form-control"
-                    required
-                  />
-                </div>
+
+              <div className='col-sm-6 mt-4'>
+                <CommonInput
+                  Label={'Last Name'}
+                  Name={'lname'}
+                  PlaceHolder={'Enter your last name'}
+
+                />
               </div>
-              <div className="row mb-2">
-                <div className="col-lg-3 col-md-6 col-sm-12">
-                  <div className="form-outline mb-2">
-                    <label className="form-label" htmlFor="inputAddAttribute-value">
-                      Last Name
-                    </label>
-                    <input
-                      type="text"
-                      id="inputAddAttribute-value"
-                      className="form-control"
-                      required
-                    />
-                  </div>
-                </div>
+
+            </div>
+
+            <div className="row mt-4">
+              <div className='col-sm-6 mt-4'>
+                <CommonInput
+                  Label={'User Name'}
+                  Name={'Uname'}
+                  PlaceHolder={'Enter your User name'}
+
+                />
+              </div>
+
+              <div className='col-sm-6 mt-4'>
+                <CommonInput
+                  Label={'Country'}
+                  Name={'country'}
+                  PlaceHolder={'Enter your country'}
+
+                />
               </div>
             </div>
+            <div className='row mt-4'>
+
+              <CommonInput
+                Label={'Email'}
+                Name={'email'}
+                PlaceHolder={'mymail@gmail.com'}
+
+              />
+
+            </div>
+            <div className='row mt-4'>
+
+              <CommonInput
+                Label={'Short Description'}
+                Name={'shortDescription'}
+                PlaceHolder={'Enter your words'}
+                TextArea={true}
+
+              />
+
+            </div>
+            <div className='row mt-4'>
+              <CommonInput
+                Label={'Profile picture'}
+                Name={'profilePicture'}
+                Type={"file"}
+
+              />
+
+            </div>
+            <div className='row mt-4'>
+
+              <CommonInput
+                Label={'Upload Agreements'}
+                Name={'upload'}
+                Type={"file"}
+
+              />
+
+            </div>
+
+            <div className='row mt-4 mb-5'>
+            <button type="button" className="btn add-customer-button" style={{width:'auto'}}>Add Customer</button>
+            </div>
           </div>
+
         </div>
+
       </div>
     </Layout>
   )
---
+++ frontend/src/components/common/CommonInput/CommonInput.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const CommonInput = ({ Label, Type, Name, Onchange, PlaceHolder, Disable, TextArea }) => {
+    return (
+        <div>
+            <label className="form-label" htmlFor="inputAddAttribute-value">
+                {Label}
+            </label>
+
+            {TextArea ? (
+                <textarea
+                    className="form-control"
+                    rows="3"
+                    name={Name}
+                    onChange={Onchange}
+                    placeholder={PlaceHolder}
+                    disabled={Disable}
+                    required
+
+                />
+            ) : (
+                <input
+                    type={Type}
+                    name={Name}
+                    className="form-control"
+                    onChange={Onchange}
+                    placeholder={PlaceHolder}
+                    disabled={Disable}
+                    required
+                />
+            )}
+
+        </div>
+    )
+}
+
+export default CommonInput
